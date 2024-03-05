f2310bc041b11c3125bced4dbe912d6de83aec99
Phani Raj <phani.blrtraining@gmail.com>
2024-03-05 19:38:26 +0530

    Add project files.

---
+++ ChimeBlazorApp/Services/ApiService.cs
@@ -0,0 +1,29 @@
+﻿// ApiService.cs
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class ApiService
+{
+    private readonly HttpClient httpClient;
+    private const string baseUrl = "https://pulud6u8je.execute-api.us-east-1.amazonaws.com/Prod/create?m=123";
+
+    public ApiService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task<string> GetApiResponse()
+    {
+        // Replace "your-api-endpoint" with the actual endpoint of your REST API
+        var response = await httpClient.GetAsync(baseUrl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            return "Error fetching data from the API";
+        }
+    }
+}
